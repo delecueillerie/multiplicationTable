f64f786bb878421988377f520e1b55a6af23ef62
Lagspoon <lagspoon@gmail.com>
2014-12-19 16:55:17 +0100

    Initial Commit

---
+++ multiplicationTable/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  multiplicationTable
+//
+//  Created by Olivier Delecueillerie on 19/12/2014.
+//  Copyright (c) 2014 lagspoon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ multiplicationTable/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  multiplicationTable
+//
+//  Created by Olivier Delecueillerie on 19/12/2014.
+//  Copyright (c) 2014 lagspoon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
